426bc0d628151dd78724b0c3392e65c114737476
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-10-16 15:30:11 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Song.create([
+    {title: "7 Rings", released: true, release_year: 2018, artist_name: "Ariana Granda", genre: "Pop"},
+    {title: "Just Friends", released: true, release_year: 2018, artist_name: "Hayden James", genre: "Techno"},
+    {title: "Vossi Bop", released: true, release_year: 2019, artist_name: "Stormzy", genre: "Grime"}
+])
